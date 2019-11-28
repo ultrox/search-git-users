bcba5cd2ee7223d6ee73b1d7ec449eeb3a7a03c3
Marko <crashxx@gmail.com>
2019-11-28 01:37:34 +0100

    Add more refined dropdown

--- src/GithubSearchUsers/index.js
+++ src/GithubSearchUsers/index.js
@@ -1,20 +1,46 @@
 import React from 'react'
-import {SearchStyle} from '../styles'
+import {
+  SearchStyle,
+  DropDown,
+  DropDownItem,
+  ItemAvatar,
+  ItemUsername,
+} from '../styles'
 
 const PLACEHOLDER = 'Search for users'
 
 function GithubSearchUsers() {
   return (
-    <SearchStyle>
-      <input
-        type="search"
-        spellCheck="false"
-        autocapitalization="none"
-        autoComplete="off"
-        autoCorrect="off"
-        placeholder={PLACEHOLDER}
-      />
-    </SearchStyle>
+    <>
+      <SearchStyle>
+        <input
+          type="search"
+          spellCheck="false"
+          autocapitalization="none"
+          autoComplete="off"
+          autoCorrect="off"
+          placeholder={PLACEHOLDER}
+        />
+      </SearchStyle>
+      <DropDown>
+        <DropDownItem
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/ultrox"
+        >
+          <ItemAvatar src="https://api.adorable.io/avatars/79/abott1@adorable.png" />
+          <ItemUsername>marko</ItemUsername>
+        </DropDownItem>
+        <DropDownItem
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/ultrox"
+        >
+          <ItemAvatar src="https://api.adorable.io/avatars/79/abott6@adorable.png" />
+          <ItemUsername>marko</ItemUsername>
+        </DropDownItem>
+      </DropDown>
+    </>
   )
 }
 
