939cacaa05d2a48fc2613abe1002c35fb017837e
Marko <crashxx@gmail.com>
2019-11-28 01:47:56 +0100

    Add media query for wrapper

--- src/styles/elements.js
+++ src/styles/elements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import {screens} from './settings'
 
 export const AppWrapper = styled.div`
   font-size: 2rem;
@@ -7,6 +8,9 @@ export const AppWrapper = styled.div`
 export const MainWrapper = styled.div`
   border-radius: 1rem;
   background-color: #f4f6f9;
-  width: 40%;
+  width: 80%;
+  @media (min-width: ${screens.lg}) {
+    width: 40%;
+  }
   margin: 0 auto;
 `
